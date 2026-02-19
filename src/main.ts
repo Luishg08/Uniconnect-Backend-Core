0e856c8e8d6c23a7007c3481e1a047eb033d2fbc
Luis Henao <lhenao@consensussa.com>
2026-02-18 19:33:57 -0500

    feat: adding docs with swagger-ui and environment configuration

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>('PORT') || 3000;
+  app.setGlobalPrefix('api');
+  const config = new DocumentBuilder()
+    .setTitle('Uniconnect API')
+    .setDescription('Documentación oficial de la API Uniconnect')
+    .setVersion('1.0')
+    .addBearerAuth() // para JWT después
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('docs', app, document);
+  await app.listen(port);
 }
 bootstrap();
