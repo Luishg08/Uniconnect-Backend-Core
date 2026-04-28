a1c1e9f61aa2b8cb24bc3683baa308c9ae1abf5b
Luis Henao <lhenao@consensussa.com>
2026-04-27 21:22:32 -0500

    chore: fix api limit

--- src/main.ts
+++ src/main.ts
@@ -31,8 +31,6 @@ async function bootstrap() {
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
-  app.use(express.json({ limit: '10mb' }));
-  app.use(express.urlencoded({ limit: '10mb', extended: true }));
   await app.listen(port);
 }
 bootstrap();
